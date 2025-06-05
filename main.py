58e6ac4381f8e533f92170aa08645cd3b7360c98
Jyoti Vishwakarma <65598611+jyotivish08@users.noreply.github.com>
2025-06-05 12:49:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def handle_login():
     email = email_input.get()
     password = password_input.get()
 
-    if email== 'jyoti@gmail.com' and password == '1234':
+    if email== 'abc@gmail.com' and password == '1234':
         messagebox.showinfo('yayy', 'Login successful')
     else:
         messagebox.showerror('Error', 'Login Failed')
